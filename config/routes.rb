9aff473eda33e0893bd182dcfa241f27a308b08e
Kee Ga Jin <gajinkee@gmail.com>
2023-12-17 23:15:43 +0800

    userlogin and username with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "/posts", to: "posts#index"
