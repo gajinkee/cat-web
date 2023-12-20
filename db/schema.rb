c7d1a9113713c48d2b7256ed0c4b0e34931a0b4b
Kee Ga Jin <gajinkee@gmail.com>
2023-12-21 02:30:48 +0800

    shifted to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_17_155109) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "public"
@@ -27,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_17_155109) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "public"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
